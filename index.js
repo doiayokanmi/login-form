382613d1ee2fdcdffc4a8e06b7854313a1e8decb
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-03-18 09:27:02 -0700

    JS file initiateed

---
+++ index.js
@@ -0,0 +1,9 @@
+var mainSec = document.getElementById('mainSec');
+var asideSec = document.getElementById('asideSec');
+var signupSec = document.getElementById('signup');
+
+function slide() {
+    mainSec.classList.add('slideRight');
+    asideSec.classList.add('slideLeft');
+    signupSec.classList.add('slideRights')
+}
